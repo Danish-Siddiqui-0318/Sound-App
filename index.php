17a2033570d430e1fda331aaaebc9288328669c8
Danish-Siddiqui-0318 <contact.dasiddique@gmail.com>
2024-07-14 01:04:16 +0500

    Manage frontend

--- index.php
+++ index.php
@@ -32,35 +32,8 @@
       <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
    </head>
    <body>
-      <!-- header section start -->
-      <div class="header_section">
-         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="logo" href="index.html"><img src="images/logo.png"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-               <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                     <a class="nav-link" href="index.html">Home</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="movies.html">Movies</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="tv.html">TV</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="celebs.html">Celebs</a>
-                  </li>
-               </ul>
-               <div class="search_icon"><a href="#"><img src="images/notification-icon.png"><span class="padding_left_15">Notificastion</span></a></div>
-               <div class="search_icon"><a href="#"><img src="images/eye-icon.png"><span class="padding_left_15">Viwe</span></a></div>
-               <div class="search_icon"><a href="#"><img src="images/user-icon.png"><span class="padding_left_15">login</span></a></div>
-               <div class="search_icon"><a href="#"><img src="images/search-icon.png"><span class="padding_left_15">Search...</span></a></div>
-            </div>
-         </nav>
-      </div>
+      <!-- header section start --> 
+      <?php include_once('component/navbar.php') ?>
       <!-- header section end -->
       <!-- banner section end -->
       <div class="banner_section layout_padding">
