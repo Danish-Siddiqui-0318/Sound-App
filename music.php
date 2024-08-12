9b4d14c49bd74b97465fa52d2f0e2206119af9b9
Danish-Siddiqui-0318 <contact.dasiddique@gmail.com>
2024-08-12 13:49:22 +0500

    Fix some error

--- music.php
+++ music.php
@@ -51,7 +51,7 @@
                         <input placeholder="Search..." type="text" name="query" value="<?php if (isset($_GET['query'])) {
                                                                                             echo $_GET['query'];
                                                                                         } ?>">
-                        <button type="submit" style="background-color: green;">Go</button>
+                        <button type="submit" style="background-color: green;transition: 0.4s;">Go</button>
                     </div>
                 </form>
             </div>
